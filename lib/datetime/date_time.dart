5037e8b8f7e3daac17a533378a0e76f2e248bdd3
Laxman Bhattarai <erluxman@gmail.com>
2020-01-08 09:45:07 +0545

    fixing ci

--- lib/datetime/date_time.dart
+++ lib/datetime/date_time.dart
@@ -1,12 +1,3 @@
-//extension DateTimeExtensions on DateTime {
-//  String toStringYYYYMMDD({String separator = "-"}) {
-//    return "${this.year.toString()}$separator${this.year.toString()}$separator${this.year.toString()}";
-//  }
-//  String toStringMMDD({String separator = "-"}) {
-//    return "${this.year.toString()}$separator${this.year.toString()}";
-//  }
-//}
-
 import 'package:flutterx/utils/numbers.dart';
 
 extension DateTimeExtensions on DateTime {
